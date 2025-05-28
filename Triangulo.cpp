daf163afb43560a3789d5f56566c0019bf68ed53
ChristianUrie1516 <uriel.gzz.flores@gmail.com>
2025-05-28 07:01:33 -0600

    Add files via upload

---
+++ Triangulo.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <cmath> // Para usar sqrt()
+// Christian Uriel Gonzalez Flores 362423176
+using namespace std;
+
+int main() {
+	float a, b, c, perimetro;
+	
+	cout<< "Calculo del perimetro de un triangulo rectangulo" << endl; 
+	cout<< "Dame el cateto opuesto: ";
+	cin >> a;
+	cout<< "Dame el cateto adyacente: ";
+	cin >> b;
+	
+	//Calculo de la hipotenusa
+	c = sqrt((a*a)+(b*b));
+	
+	//Calculo del perimetro
+	perimetro = a + b + c;
+	
+	cout << "La hipotenusa es: " << c << endl;
+	cout << "El perimetro del triangulo es: " << perimetro << endl;
+	
+	return 0;
+
+}
